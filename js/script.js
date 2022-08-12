bfb6a940cc48c4a6747edb46dab4f74ef037fb61
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-12 13:57:47 +0300

    added task #2

--- js/script.js
+++ js/script.js
@@ -3,23 +3,23 @@
 
 //////////      Task #1
 
-function sumTwoSmallestNumbers (...array) {
+// function sumTwoSmallestNumbers (...array) {
 
-    let sortArray = array.sort((a, b) => a - b);
+//     let sortArray = array.sort((a, b) => a - b);
 
-    let x = sortArray[0];
-    let y = sortArray[1];
+//     let x = sortArray[0];
+//     let y = sortArray[1];
 
-    if (array.length <= sortArray) {
-        return console.error('array is smoll');
-    } else {
-        return x + y;
-    }
+//     if (array.length <= sortArray) {
+//         return console.error('array is smoll');
+//     } else {
+//         return x + y;
+//     }
 
-}
+// }
 
-console.log(sumTwoSmallestNumbers(2,3,5,1,7,8));
-console.log(sumTwoSmallestNumbers(2));
+// console.log(sumTwoSmallestNumbers(2,3,5,1,7,8));
+// console.log(sumTwoSmallestNumbers(2));
 
 
 /////////
@@ -27,3 +27,26 @@ console.log(sumTwoSmallestNumbers(2));
 ////////        Task #2
 
 
+function makeCalc (calc) {
+    return {
+        sum: function(sum) {
+            return calc = calc + sum;
+        },
+        mult: function(mult) {
+            return calc = calc * mult;
+        },
+        sub: function(sub) {
+            return calc = calc - sub;
+        },
+        div: function(div) {
+            return calc = calc / div;
+        },
+    }
+}
+
+const calculete = makeCalc(10);
+
+console.log(calculete.sum(5));
+console.log(calculete.mult(10));
+console.log(calculete.sub(40)); 
+console.log(calculete.div(10)); 
