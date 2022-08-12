e18626146d6cd13ab03cf2e8ab805d5864f8b4cb
Vlad Yefremenko <vladyefremenko@Vlads-MacBook-Pro.local>
2022-08-12 11:13:06 +0300

    added checked array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+//////////      Task #1
+
+function sumTwoSmallestNumbers (...array) {
+
+    let sortArray = array.sort((a, b) => a - b);
+
+    let x = sortArray[0];
+    let y = sortArray[1];
+
+    if (array.length <= sortArray) {
+        return console.error('array is smoll');
+    } else {
+        return x + y;
+    }
+
+}
+
+console.log(sumTwoSmallestNumbers(2,3,5,1,7,8));
+console.log(sumTwoSmallestNumbers(2));
+
+
+/////////
+
+////////        Task #2
+
+
